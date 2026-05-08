626275773ae2252c3529b1b2bf6283b28482568e
Py-Ru <ashvinmaruti@gmail.com>
2026-05-08 13:12:49 +0100

    Implement comment system with permissions

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import PostDetailView, PostListCreateView
+from .views import (
+    PostDetailView,
+    PostListCreateView,
+    CommentListCreateView,
+    CommentDetailView,
+)
 
 urlpatterns = [
     path('', PostListCreateView.as_view()),
     path('<slug:slug>/', PostDetailView.as_view()),
+    path('<slug:slug>/comments/', CommentListCreateView.as_view()),
+    path('comments/<int:pk>/', CommentDetailView.as_view()),
 ]
